9968e11a4e6bb8791b2cb708050bc1381a6ccb8f
Noah Blumenstein <noah.blumenstein@gmail.com>
2022-08-03 09:12:09 -0400

    basic functionality working

---
+++ index.js
@@ -0,0 +1,72 @@
+require('dotenv').config()
+const needle = require("needle")
+
+let followersCsv = []
+
+// change depending on id of account we want to grab followers from
+const userId = "1267191132200591362"
+const url = `https://api.twitter.com/2/users/${userId}/followers`;
+const bearerToken = process.env.TWITTER_BEARER_TOKEN
+
+async function getFollowers () {
+    let followers = []
+    let params = {
+        "max_results": 1000,
+    }
+
+    const options = {
+        headers: {
+            "User-Agent": "bean3ntoast",
+            "authorization": `Bearer ${bearerToken}`
+        }
+    }
+
+    let hasNextPage = true;
+    let nextToken = null;
+    console.log("Grabbing followers... sauce")
+
+    while (hasNextPage) {
+        let resp = await getPage(params, options, nextToken)
+        if (resp && resp.meta && resp.meta.result_count && resp.meta.result_count > 0) {
+            if (resp.data) {
+                followers.push.apply(followers, resp.data);
+            }
+            if (resp.meta.next_token) {
+                nextToken = resp.meta.next_token;
+            } else {
+                hasNextPage = false;
+            }
+        } else {
+            hasNextPage = false;
+        }
+    }
+
+    console.log(followers)
+    console.log(`Retreived ${followers.length} users`)
+
+    // convert to csv
+
+
+}
+
+
+
+async function getPage (params, options, nextToken) {
+    if (nextToken) {
+        params.pagination_token = nextToken;
+    }
+
+    try {
+        const resp = await needle('get', url, params, options);
+
+        if (resp.statusCode != 200) {
+            console.log(`${resp.statusCode} ${resp.statusMessage}:\n${resp.body}`);
+            return;
+        }
+        return resp.body;
+    } catch (err) {
+        throw new Error(`Request failed: ${err}`);
+    }
+}
+
+getFollowers();
